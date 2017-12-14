9fd178b634159d89adaa5ac682ef34d3d5324d18
nipunthathsara <nipunthathsara@gmail.com>
2017-12-14 11:01:52 +0530

    sub attribute iteration added

--- src/main/java/org/wso2/confvalidator/APIManagerValidator.java
+++ src/main/java/org/wso2/confvalidator/APIManagerValidator.java
@@ -1,5 +1,7 @@
 package org.wso2.confvalidator;
 
+import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.w3c.dom.Document;
 import org.wso2.confvalidator.org.wso2.confvalidator.utils.Constants;
@@ -28,29 +30,73 @@ public class APIManagerValidator {
         jsons = (new JSONReader()).loadJsons();
     }
 
+    /**
+     * Method to iterate configurations
+     */
     public void iterator() {
         JSONObject apiManagerKB = jsons.get(Constants.API_MANAGER_XML);
         Iterator<?> keySet = apiManagerKB.keySet().iterator();
         while (keySet.hasNext()) {
             String key = (String) keySet.next();
-            if ( apiManagerKB.get(key) instanceof JSONObject ) {
+            if (apiManagerKB.get(key) instanceof JSONObject) {
                 validateApiMangerCommonConfigs((JSONObject) apiManagerKB.get(key));
             }
         }
     }
 
+    /**
+     * TODO remove overloading. keep only one
+     * Method to re-iterate sub configurations
+     *
+     * @param configurations
+     */
+    public void iterator(JSONObject configurations) {
+        Iterator<?> keySet = configurations.keySet().iterator();
+        while (keySet.hasNext()) {
+            String key = (String) keySet.next();
+            if (configurations.get(key) instanceof JSONObject) {
+                validateApiMangerCommonConfigs((JSONObject) configurations.get(key));
+            }
+        }
+    }
+
     public void validateApiMangerCommonConfigs(JSONObject configuration) {
-        if(configuration.containsKey("options")){
+        //Does it contain sub attributes? If so, iterate again
+        if (configuration.containsKey("options")) {
+            iterator((JSONObject) configuration.get("options"));
             return;
         }
+        //Get value in XML
         String xpath = (String) configuration.get("xpath");
         String value = (new XpathEvaluator()).evaluateXpath(this.apiManagerXML, xpath, XPathConstants.STRING).toString();
 
         //Mandatory Check
-        if("yes".equals(configuration.get("mandatory")) &&  "".equals(value)){
+        if (configuration.containsKey("mandatory") && "yes".equals(configuration.get("mandatory")) && "".equals(value)) {
             System.out.println(xpath + "Field is required");
         }
 
+        //Default check
+        if (configuration.containsKey("default") && configuration.get("default").toString().equals(value)) {
+            System.out.println(xpath + "is using DEFAULT value");
+        }
 
+        //Cross Reference check
+        if (configuration.containsKey("crossReference")) {
+            JSONObject referenceArray = (JSONObject) configuration.get("crossReference");
+            //TODO cross reference logic here
+        }
+
+        //Parsable values
+        if (configuration.containsKey("parsableValues")) {
+            JSONArray parsableValues = (JSONArray) configuration.get("parsableValues");
+            boolean validValue = false;
+            for (int i = 0; i < parsableValues.size(); i++) {
+                if (value.equals(parsableValues.get(i))) {
+                    validValue = true;
+                    break;
+                }
+            }
+            System.out.println("Entered value is valid: " + validValue);
+        }
     }
 }
